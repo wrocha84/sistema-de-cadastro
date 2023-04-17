df7f1ad41133c15f6bb60e7bf42cbb0dfe931c4e
William Rocha <williamacbr@fazenda.sp.gov.br>
2023-04-17 09:54:55 -0300

    adicionando mais um console log

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("A aplicação está se conectando ao banco")
+console.log("O Paulo fez esse commit sem vc ver, durante a aula!")
+console.log("Enviando para o GitHub")
